ad394926fc2cdd16c90476cbb908170d3d557048
Yousquid <ly2456@nyu.edu>
2026-01-21 16:44:18 -0500

    Class_1

--- Game_Feel/Assets/PlayerMovement.cs
+++ Game_Feel/Assets/PlayerMovement.cs
@@ -4,15 +4,60 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    // Start is called before the first frame update
+    private Vector3 velocity;
+
+    [SerializeField] private float speed;
+    [SerializeField] List<Transform> wallList = new List<Transform>();
+
+
     void Start()
     {
         
     }
 
-    // Update is called once per frame
     void Update()
     {
         
+    }
+
+    private void FixedUpdate()
+    {
+        velocity = Vector3.zero;
+
+        if (Input.GetKey(KeyCode.W)) velocity.y += 1;
+        if (Input.GetKey(KeyCode.S)) velocity.y += -1;
+        if (Input.GetKey(KeyCode.A)) velocity.x += -1;
+        if (Input.GetKey(KeyCode.D)) velocity.x += 1;
+
+        velocity = velocity.normalized;
+        velocity *= speed;
+        velocity *= Time.fixedDeltaTime;
+
+
+        Vector3 intendedNextPosition = transform.position + velocity;
+        if (!IsThisPlayerInAWall(intendedNextPosition))
+        {
+            transform.position += velocity;
+        }
+    }
+
+    bool IsThisPlayerInAWall(Vector3 positionCheck)
+    {
+        foreach (Transform currentWall in wallList)
+        {
+            //Calculate the distance between the player and the wall in X and in Y
+            float xDistance = Mathf.Abs(positionCheck.x - currentWall.position.x);
+            float yDistance = Mathf.Abs(positionCheck.y - currentWall.position.y);
+
+            float xMaxDistance = transform.localScale.x / 2 + currentWall.localScale.x / 2;
+            float yMaxDistance = transform.localScale.y / 2 + currentWall.localScale.y / 2;
+
+            if (xDistance < xMaxDistance && yDistance < yMaxDistance)
+            {
+                return true;
+            }
+
+        }
+        return false;
     }
 }
